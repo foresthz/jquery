a713e29dd556c95e7a44b2fd7ec3fcb2ee64a48f
forest <forest@tom.com>
2015-05-18 02:33:25 +0800

    ok: ok

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,20 @@
 var debug = true;
 
 var gulp = require('gulp');
-var jsonTool = require('json-utils');
+  jsonTool = require('json-utils');
+  // loadTasks = require('gulp-load')(gulp);
+
+
+// 不能从本地加载文件模块?
+// 模块加载细节,还需要练习
+// require('./build/otask/mystask.js');
 
 gulp.task("test", function(){
   debug = true;
   console.info('test ...');
   var json = jsonTool.readJSONSync('./src/.jshintrc');
-
   // var json = require('./src/.jshintrc');
-
 });
 
 
+// loadTasks('build/gulp_task/mytask.js');
